143c7e245af39071941a101297e23ef9c77a4e1c
VinehNxp <vineh2006@gmail.com>
2025-10-05 17:56:53 -0300

    sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,26 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request));
 });
+
+const cacheName = 'salonai-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
